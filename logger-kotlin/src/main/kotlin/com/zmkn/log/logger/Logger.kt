88324bba9dabcbfe459197f8aad74daae0e13038
hz <beijingren@vip.qq.com>
2025-06-03 10:02:34 +0800

    调整 Logger 的逻辑

--- logger-kotlin/src/main/kotlin/com/zmkn/log/logger/Logger.kt
+++ logger-kotlin/src/main/kotlin/com/zmkn/log/logger/Logger.kt
@@ -7,8 +7,8 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-class Logger private constructor(config: Config) {
-    private val _level = getLevel() ?: config.level
+class Logger private constructor(private val config: Config) {
+    private val _level: Level get() = getLevel() ?: config.level
     private val _stackIndex = STACK_INDEX + (getStackOffset() ?: config.stackOffset)
 
     private fun getAnsiColor(level: Level): AnsiColor {
